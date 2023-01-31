e87d8d9d0f9239338c349d6485e01470a834cfda
MaximeBritto <90254429+MaximeBritto@users.noreply.github.com>
2023-01-31 11:34:28 +0100

    projetAddFile

--- projethe/src/App.js
+++ projethe/src/App.js
@@ -1,16 +1,17 @@
 import Home from "./pages/Home";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-
+import Produit from "./pages/Produit";
 
 function App() {
   return (
     <BrowserRouter>
-    <Routes>
-      <Route path="/" element={<Home />} />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/produit" element={<Produit />} />
 
-      <Route path="*" element={<Home />} />
-    </Routes>
-  </BrowserRouter>
+        <Route path="*" element={<Home />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
