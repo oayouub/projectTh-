d1cfc3e4f82684a8a89bf9cc7a7e799bdaf07709
oayouub <91758300+oayouub@users.noreply.github.com>
2023-01-31 21:09:44 +0100

    homepage progress

--- projethe/src/pages/Home.js
+++ projethe/src/pages/Home.js
@@ -6,6 +6,35 @@ const Home = () => {
     return (
         <div>
            <Navigation />
+           <div className="sectionBanniere">
+            <img src="./img/BANNIERE.png" alt="img" />
+            <button>DÉCOUVRIR</button>
+            </div>
+
+            <div className="categorieDiv">
+                <h2>CATÉGORIES</h2>
+                <div className="categorie-filter">
+                    <ul>
+                        <li>
+                            <img src="./img/imgFilter.jpg" alt="img" />
+                            <h4>NOIR</h4>
+                        </li>
+                        <li>
+                        <img src="./img/imgFilter.jpg" alt="img" />
+                            <h4>BLANC</h4>
+                        </li>
+                        <li>
+                        <img src="./img/imgFilter.jpg" alt="img" />
+                            <h4>VERT</h4>
+                        </li>
+                        <li>
+                        <img src="./img/imgFilter.jpg" alt="img" />
+                            <h4>JAUNE</h4>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+
         </div>
     );
 };
